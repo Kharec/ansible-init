0097f67378c1008b884b09e024041eb619f219c2
Sandro CAZZANIGA <sandro@cazzaniga.fr>
2018-07-04 18:58:33 +0200

    Create ansible.cfg ourself

--- ansible-init.sh
+++ ansible-init.sh
@@ -15,10 +15,16 @@ mkdir bin env lib play ssh roles
 touch README.md ansible.cfg requirements.yml ssh/config env/inventory.ini main.yml
 mkdir env/group_vars env/host_vars
 
+echo "[defaults]
+inventory = env/inventory.ini
+roles_path = roles
+
+[ssh_connection]
+ssh_args = -F ssh/config -C -o ControlMaster=auto -o ControlPersist=60s" > ansible.cfg
+
 echo "Don't forget to : 
 
 - add your keys in ssh/
-- add your configuration in ansible.cfg
 - add your servers and groups in env/inventory.ini"
 
 exit 0
